f55e4e2a7f998ab5df1fa92f936aa79c3145e9a5
mustangfpr <fedorecpiskunov@gmail.com>
2026-03-03 12:05:37 +0300

    Initial version of array program

---
+++ array.py
@@ -0,0 +1,17 @@
+arr = []
+n = int(input("Enter number of elements (max 15): "))
+if n > 15:
+    print("Too many elements, set to 15")
+    n = 15
+print("Enter elements:")
+i = 0
+while i < n:
+    tmp = int(input(""))
+    arr.append(tmp)
+    i += 1
+print("Array:")
+i = 0
+while i < n:
+    print(arr[i], end=" ")
+    i += 1
+print("")
